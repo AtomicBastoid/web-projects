642a26f941cc195f03835846ac552e9b0008336d
Eishal Keshwani <keshwanieishal001@gmail.com>
2025-04-13 22:36:58 +0500

    Added ability to delete posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('posts/', views.posts, name='posts'),
-    path('posts/create', views.create, name='create')
+    path('posts/create', views.create, name='create'),
+    path('posts/my_posts', views.my_posts, name="my_posts")
 ]
--- blog/views.py
+++ blog/views.py
@@ -26,3 +26,20 @@ def create(request):
 
     return render(request, 'blog/create.html', {"form": form})
 
+@login_required
+def my_posts(request):
+    posts = Post.objects.filter(author=request.user).all()
+    if request.method == "POST":
+        post_id = request.POST.get("post-id")
+        print(post_id)
+        
+        if post_id:
+            post = Post.objects.get(id=post_id)
+            if post and (post.author == request.user):
+                post.delete()
+                
+        posts = Post.objects.all()
+
+    return render(request, 'blog/my_posts.html', {
+        'posts': posts
+    })
